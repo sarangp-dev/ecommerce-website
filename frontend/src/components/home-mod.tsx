a8bafc114f99ed4e9c3908f6fa17856ab0f08634
Sarang <sarang.p1230@gmail.com>
2026-08-19 14:48:04 +0530

    time added 10sec

--- frontend/src/components/home-mod.tsx
+++ frontend/src/components/home-mod.tsx
@@ -297,7 +297,7 @@ function HeroSection({ products }: { products: any[] }) {
 
         const timer = setTimeout(() => {
             setCurrentImage((prev) => (prev + 1) % products.length);
-        }, 5000);
+        }, 10000);
 
         return () => clearTimeout(timer);
     }, [products, currentImage]);
