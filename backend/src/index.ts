8eefe3252e9dd7a9b9f149eb1bfeeeab52442a9c
Sarang <sarang.p1230@gmail.com>
2026-08-21 12:34:31 +0530

    CORS configuration.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,13 +9,27 @@ import connectDB from "./config/db.js";
 import cloudinary from "./config/cloudinary.js";
 import cookieParser from "cookie-parser";
 
+const allowedOrigins = (process.env.FRONTEND_URL || "")
+    .split(",")
+    .map(origin => origin.trim());
 dotenv.config();
 
 connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/user", userRoutes)
